cb063b2c01638fbf6a8bacb4daaca89a8c0f17b2
defiance99 <var.alo@mail.ru>
2021-04-16 00:01:46 +0300

    initial

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Get, Injectable, Param, Res } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello WWorld!';
   }
 }
