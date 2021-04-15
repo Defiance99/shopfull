86c7fe6e081a93a88e7c05f2682696d51e74d9d4
defiance99 <var.alo@mail.ru>
2021-04-15 20:10:15 +0300

    initial

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.use('/uploads', express.static('uploads'));
 
   await app.init();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
