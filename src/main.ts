1623e3c096762cb6a2fb11d61f7aa1aa416000d5
defiance99 <var.alo@mail.ru>
2021-04-17 04:07:28 +0300

    auto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
-import { Request, Response } from 'express';
-import path, { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
